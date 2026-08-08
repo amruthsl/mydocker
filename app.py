6b627f4fb84ee36a88203e6e50edd8b5c4e1e5e7
amruthsl <amruthsl@gmail.com>
2026-08-08 20:30:58 +0200

    Enhance home route and add /api/info endpoint
    Updated home route to return HTML content and added a new API endpoint for status information.

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello amruth, your Docker + Jenkins CI/CD is working!"
+    return """
+    <h1 style='color: green;'>Hello Amruth!</h1>
+    <p>Your Jenkins + Docker CI/CD pipeline is working perfectly.</p>
+    <p>Every commit will now auto‑deploy.</p>
+    """
+
+@app.route("/api/info")
+def info():
+    return jsonify({
+        "status": "success",
+        "message": "CI/CD pipeline is running",
+        "author": "amruth"
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
